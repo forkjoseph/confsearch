f6ce9c0d15412c8d585cc5ac3195c39a068a0021
Joseph <forkjoseph@gmail.com>
2017-12-12 17:15:38 -0500

    packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='ConfSearch',
+    version='0.1dev',
+    packages=['confsearch',],
+    license='GPLv2',
+    long_description=open('README.txt').read(),
+)
+
